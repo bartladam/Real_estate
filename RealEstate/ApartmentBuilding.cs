b6ac9caa8bf01caaf499e4d703b827c532996f47
Adam46072 <adam.bartl2001@seznam.cz>
2023-09-10 22:09:07 +0200

    Apartment without flats complete

--- RealEstate/ApartmentBuilding.cs
+++ RealEstate/ApartmentBuilding.cs
@@ -8,5 +8,14 @@ namespace RealEstate
 {
     internal class ApartmentBuilding
     {
+        public string nameApartment { get; private set; }
+        public List<Flat> flats { get; set; }
+        public ApartmentBuilding(string nameApartment, params Flat[] flats)
+        {
+            this.nameApartment = nameApartment;
+            this.flats = new List<Flat>(flats);
+        }
+
+
     }
 }
