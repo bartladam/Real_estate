1719825f403c3470c47ab6fdbf54ae62b7d0f0a6
Adam46072 <adam.bartl2001@seznam.cz>
2023-09-10 22:00:05 +0200

    System complete without connection

--- RealEstate/System.cs
+++ RealEstate/System.cs
@@ -8,5 +8,25 @@ namespace RealEstate
 {
     internal class System
     {
+        public List<ApartmentBuilding> ApartmentBuildings { get; private set; }
+        public List<Invoice> Invoices { get; private set; }
+        Administrator Administrator;
+        public System()
+        {
+            this.ApartmentBuildings = new List<ApartmentBuilding>();
+            this.Invoices = new List<Invoice>();
+        }
+        public string FlatOffer(int order)
+        {
+
+        }
+        public Flat SelectedFlat(string indicationFlat)
+        {
+
+        }
+        public void AvailabilityFlat()
+        {
+
+        }
     }
 }
