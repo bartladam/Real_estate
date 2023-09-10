64ff4f34490e3e781c76f5160ec7eb4972f013c0
Adam46072 <adam.bartl2001@seznam.cz>
2023-09-10 22:13:28 +0200

    flat complete without connection

--- RealEstate/Flat.cs
+++ RealEstate/Flat.cs
@@ -8,5 +8,21 @@ namespace RealEstate
 {
     internal class Flat
     {
+        public bool avaible { get; private set; } = true;
+        public uint priceFlat { get; private set; }
+        public int sizeFlat { get; private set; }
+        public int floorFlat { get; private set; }
+        public string indicationFlat { get; private set; }
+        public Flat(uint priceFlat, int sizeFlat, int floorFlat, string indicationFlat)
+        {
+            this.priceFlat = priceFlat;
+            this.sizeFlat = sizeFlat;
+            this.floorFlat = floorFlat;
+            this.indicationFlat = indicationFlat;
+        }
+        public void BuyFlat()
+        {
+            avaible = false;
+        }
     }
 }
